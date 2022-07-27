ebcac15895fe4daeefd2f9fa7606072eda23c859
Daniel Afanador <danielthemisfits@gmail.com>
2022-07-26 21:54:06 -0300

    Ejemplos de cómo usar la función round()

---
+++ round-exercise.py
@@ -0,0 +1,11 @@
+one_decimal = round(3.43, 1)
+
+two_decimals = round(0.23321, 2)
+
+five_decimals = round(921.2225227, 5)
+
+print (one_decimal)
+print ('')
+print (two_decimals)
+print ('')
+print (five_decimals)
